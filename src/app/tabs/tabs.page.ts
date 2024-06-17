c96cf151c1806d93c0f07c792c5abf76938c62c6
Cristian Sterie <cristian.sterie@yahoo.com>
2024-06-17 14:14:33 +0300

    add fab button; prepare for userdata page;

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,24 +1,25 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonContent, IonHeader, IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar, IonButton, IonButtons, IonFab, IonFabButton, IonFabList } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { banOutline, ellipse, homeOutline, people, square, triangle } from 'ionicons/icons';
+import { banOutline, ellipse, homeOutline, people, square, triangle, person, exit } from 'ionicons/icons';
 import { ElectionService } from '../@shared/service/election.service';
 import { map } from 'rxjs';
+import { CredentialsService } from '../@shared/service/credentials.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent,],
+  imports: [IonFabList, IonFabButton, IonFab, IonButtons, IonButton, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent,],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
   electionEnabled: boolean;
 
-  constructor(private router: Router, private election: ElectionService,) {
-    addIcons({ triangle, ellipse, square, people, banOutline, homeOutline });
+  constructor(private router: Router, private election: ElectionService, private credentials: CredentialsService,) {
+    addIcons({ triangle, ellipse, square, people, banOutline, homeOutline, person, exit, });
     this.reloadPage();
   }
 
@@ -38,5 +39,10 @@ export class TabsPage {
       this.electionEnabled = res;
       console.log('(tabs) electionEnabled: ', this.electionEnabled);
     }));
+  }
+
+  logout() {
+    this.credentials.setCredentials();
+    this.router.navigate(['/login'], { replaceUrl: true });
   }
 }
