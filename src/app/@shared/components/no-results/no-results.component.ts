1237c593b19e1b7290d1435621dfa66dbc96f8cb
cristian.sterie <cristian.sterie@technohub.ro>
2025-09-15 17:30:58 +0300

    forgot to i18n;

--- src/app/@shared/components/no-results/no-results.component.ts
+++ src/app/@shared/components/no-results/no-results.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input } from '@angular/core';
-import { IonCard, IonCardContent } from "@ionic/angular/standalone";
+import { Component } from '@angular/core';
+import { IonCard, IonCardContent } from '@ionic/angular/standalone';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-no-results',
   templateUrl: './no-results.component.html',
   styleUrls: ['./no-results.component.scss'],
   standalone: true,
-  imports: [ IonCard, IonCardContent ]
+  imports: [IonCard, IonCardContent, TranslateModule],
 })
-export class NoResultsComponent {
-}
+export class NoResultsComponent {}
