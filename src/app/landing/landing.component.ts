e466bdd4eb928f8b3ad9f18c0e59b9e6f957f6a1
cristian.sterie <cristian.sterie@technohub.ro>
2025-11-03 13:17:27 +0200

    cleanup;

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,65 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import {
-  IonButton,
-  IonButtons,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCol,
-  IonContent,
-  IonGrid,
-  IonHeader,
-  IonIcon,
-  IonInput,
-  IonItem,
-  IonRow,
-  IonTitle,
-  IonToolbar,
-} from '@ionic/angular/standalone';
+import { IonButton, IonContent } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { environment } from 'src/environments/environment';
-import { CredentialsService } from '../@shared/service/credentials.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
   standalone: true,
-  imports: [
-    IonGrid,
-    IonInput,
-    IonButtons,
-    IonCardContent,
-    IonCardHeader,
-    IonRow,
-    IonCard,
-    IonCol,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonButton,
-    IonItem,
-    IonIcon,
-    FormsModule,
-    IonGrid,
-    TranslateModule,
-  ],
+  imports: [IonContent, IonButton, FormsModule, TranslateModule],
 })
 export class LandingComponent implements OnInit {
   appVersion: string;
   startAnimation = false;
 
-  constructor(private router: Router, private credentials: CredentialsService) {
+  constructor(private router: Router) {
     this.appVersion = environment.version;
   }
   ngOnInit(): void {
     setTimeout(() => {
       this.startAnimation = true;
     }, 500); // Delay before animation starts
-
   }
 
   login() {
