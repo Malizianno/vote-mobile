87720e2e286a0133d90cc1c0525b922893c2374e
cristian.sterie <cristian.sterie@technohub.ro>
2025-12-30 20:17:24 +0200

    finishing upgrade angular from 17.x to 18.x + upgraded ionic from 7.x to 8.x

--- src/app/@shared/service/generic.service.ts
+++ src/app/@shared/service/generic.service.ts
@@ -1,8 +1,4 @@
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders,
-} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CredentialsService } from './credentials.service';
 import { Router } from '@angular/router';
