f4dc51f54d8e90d97919e3b135ad835cc183b71b
cristian.sterie <cristian.sterie@technohub.ro>
2026-03-02 18:31:22 +0200

    fix kotlin sync with dependencies for building prod apks;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   production: true,
   version: packageInfo.version,
-  serverUrl: 'http://192.168.1.201:8090/vote',
+  serverUrl: 'https://vote-backend-1fk7.onrender.com/vote',
   defaultLanguage: 'ro-RO',
   supportedLanguages: ['en-US', 'ro-RO'],
 };
