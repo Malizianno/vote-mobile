5bbe007baa75b7d01459fda278a92f3b31ea18b9
cristian.sterie <cristian.sterie@technohub.ro>
2026-05-23 15:43:24 +0300

    cleanup;

--- src/app/@shared/components/home-element/home-element.component.ts
+++ src/app/@shared/components/home-element/home-element.component.ts
@@ -1,39 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import {
-  IonButton,
   IonCard,
   IonCardContent,
-  IonCol,
-  IonGrid,
-  IonIcon,
+  IonCardHeader,
+  IonCardTitle,
+  IonImg,
   IonItem,
   IonLabel,
-  IonRow,
-  IonThumbnail, IonCardTitle, IonCardHeader, IonImg } from '@ionic/angular/standalone';
+} from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { addIcons } from 'ionicons';
 import { chevronDownCircleOutline } from 'ionicons/icons';
 import { NewsfeedPost } from '../../model/newsfeed-post.model';
 
 @Component({
-    selector: 'app-home-element',
-    templateUrl: './home-element.component.html',
-    styleUrls: ['./home-element.component.scss'],
-    standalone: true,
-    imports: [IonImg, IonCardHeader, IonCardTitle, 
-        IonIcon,
-        IonCol,
-        IonRow,
-        IonCard,
-        IonItem,
-        IonButton,
-        IonCardContent,
-        IonGrid,
-        IonLabel,
-        TranslateModule,
-        CommonModule,
-    ]
+  selector: 'app-home-element',
+  templateUrl: './home-element.component.html',
+  styleUrls: ['./home-element.component.scss'],
+  standalone: true,
+  imports: [
+    IonImg,
+    IonCardHeader,
+    IonCardTitle,
+    IonCard,
+    IonItem,
+    IonCardContent,
+    IonLabel,
+    TranslateModule,
+    CommonModule,
+  ],
 })
 export class HomeElementComponent implements OnInit {
   @Input() news: NewsfeedPost;
